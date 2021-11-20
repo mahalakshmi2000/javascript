386bb2b702db480dd8ab8dc23f5a4e6f1eaed64b
mahalakshmi2000 <mahalakshmiragaviragavi@gmail.com>
2021-11-20 16:23:13 +0530

    first commit

---
+++ basics.js
@@ -0,0 +1,15 @@
+document.write("i am document");
+alert("hello i am alert ");
+console.log("hi i am console log");
+function myfunction(){
+	alert("this is my first function");
+	document.write("this is my first javascript");
+	
+}
+
+var fname = "mahalakshmi";
+var lname = "palani";
+var age = 20;
+
+
+console.log(fname+" "+lname+" "+age);
