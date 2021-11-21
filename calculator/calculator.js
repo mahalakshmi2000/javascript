2f0985116c54fd9f906d9ff658feeedf5c31ef3f
mahalakshmi2000 <mahalakshmiragaviragavi@gmail.com>
2021-11-21 20:10:38 +0530

    added calculator folder

---
+++ calculator/calculator.js
@@ -0,0 +1,43 @@
+let operator;
+let first_number;
+let second_number;
+function calculator(num){
+	console.log(num.value)
+	if(num.value == "C"){
+		document.getElementById("display").value = "";
+	}
+	else {
+		if(num.value == "+" || num.value == "-" || num.value == "*" || num.value == "/" || num.value == "%" ){
+			first_number = parseInt(document.getElementById("display").value);
+			operator = num.value;
+			document.getElementById("display").value = "";
+		}
+		
+		else if(num.value == "="){
+			
+			second_number = parseInt(document.getElementById("display").value);
+			if(operator == "+"){
+				
+				document.getElementById("display").value = first_number + second_number;
+			}
+			
+			else if(operator == "-"){
+				document.getElementById("display").value = first_number - second_number;
+			}
+			else if(operator == "*"){
+				document.getElementById("display").value = first_number * second_number;
+			}
+			else if(operator == "/"){
+				document.getElementById("display").value = first_number / second_number;
+			}
+			else if(operator == "%"){
+				document.getElementById("display").value = first_number % second_number;
+			}
+				
+			
+		}
+		else{
+			document.getElementById("display").value  += num.value;
+		}
+	}
+}
